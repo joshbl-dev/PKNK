e3adfe369a82b4775c1122752bcf6db3b1b5104e
Shubhankar Baliyan <shubhankarbaliyan16@gmail.com>
2020-09-12 05:47:21 -0400

    yoyo

--- app/src/main/java/com/hackathon/quackhacks/views/ProfileView.java
+++ app/src/main/java/com/hackathon/quackhacks/views/ProfileView.java
@@ -1,6 +1,7 @@
 package com.hackathon.quackhacks.views;
 
 import android.content.Context;
+import android.widget.EditText;
 
 import com.hackathon.quackhacks.R;
 
@@ -9,6 +10,9 @@ public class ProfileView extends BaseView {
     public ProfileView(Context context) {
         super(context);
         activity.setContentView(R.layout.profile_page);
+
+        EditText enterUser = activity.findViewById(R.id.enterUser);
+        enterUser.setText(activity.getProfile().getUsername());
 
         activity.findViewById(R.id.ExitSelfProfile).setOnClickListener( onclick -> {
             activity.changeView(new FeedView(context));
